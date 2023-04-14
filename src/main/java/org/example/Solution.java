f0a4b26c05911d16635a876cb2ea0bb7fe3a036d
Vyctrus <and.gierlak@gmail.com>
2023-04-14 21:48:49 +0200

    AHAHAHAHAHA so many troubles with unit test just to solve this only in one for loop xD

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -2,7 +2,12 @@ package org.example;
 
 public class Solution {
     public int[] solution(int[] A,int K){
-        int [] resultArray= new int[]{0,0,0};
+        int [] resultArray= new int[A.length];
+
+        for(int i=0;i<A.length;i++){
+            resultArray[(i+K)%A.length]=A[i];
+        }
+
         return resultArray;
     }
 }
